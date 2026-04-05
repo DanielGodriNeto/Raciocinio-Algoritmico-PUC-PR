d3ebeead45edbbae5a96fdb1e6430c5187124152
DanielGodriNeto <godrineto@gmail.com>
2026-04-05 20:57:25 -0300

    Juntei a

--- Calculadora PEMDAS.py
+++
@@ -1,9 +0,0 @@
-# Tarefa Calculdadora.
-# Com prioridade PEMDAS.
-
-input = input("Digite a expressão matemática: ")
-try:
-    resultado = eval(input)
-    print(f"O resultado da expressão é: {resultado}")
-except:
-    print("Expressão inválida.")
